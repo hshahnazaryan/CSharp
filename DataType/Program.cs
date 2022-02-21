76fe79cd9bcf77cef74e375a4ed90130bce0cd68
Hamlet Shahnazaryan <99588767+hshahnazaryan@users.noreply.github.com>
2022-02-21 19:41:07 +0400

    retert

--- DataType/Program.cs
+++ DataType/Program.cs
@@ -4,15 +4,15 @@ sss = sss / 8;
 string xxx = "";
 
 
-//int io = 0;
-//bool a = false;
-//string str = "dfsdf"; ;
-//Console.WriteLine(a);
-//Console.WriteLine(qq(1, 2));
+int io = 0;
+bool a = false;
+string str = "dfsdf"; ;
+Console.WriteLine(a);
+Console.WriteLine(qq(1, 2));
 
-//var stra = a.GetType();
-//var ttt = 66;
-//static int qq(int a, int b)
-//{
-//    return a + b;
-//}
+var stra = a.GetType();
+var ttt = 66;
+static int qq(int a, int b)
+{
+    return a + b;
+}
