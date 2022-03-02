17fcef2f748931ccad6e76893488aabd4f88b37e
Hamlet Shahnazaryan <99588767+hshahnazaryan@users.noreply.github.com>
2022-03-02 12:47:39 +0400

    Rekursia

--- 9-Recursion/Program.cs
+++ 9-Recursion/Program.cs
@@ -1,5 +1,9 @@
 ﻿class Program
 {
+
+    static bool prost = true;
+    static int k = 2;
+    static int n;
     static void Main(string[] args)
     {
         //Console.WriteLine((int)aaa.Monday);
@@ -11,48 +15,95 @@
         //Console.WriteLine(fbon);
 
         //Console.Write(NthFibonacciNumber(80));//2 nd wit recursion
-        //Console.ReadKey();
 
-        var t = factorial(5);
-        Console.WriteLine(t);
+        //var t = factorial(5);
+        //Console.WriteLine(t);
+        Console.WriteLine(a(1));
+        string str = Console.ReadLine();
+        PrintReverse(str);
+
+
+        Console.WriteLine("Enter number\n");
+        n = int.Parse(Console.ReadLine());
+        Func(k);
+
+        if (prost) Console.WriteLine("Prime");
+        else Console.WriteLine("Not prime");
+
         Console.ReadKey();
     }
 
-    public static int NthFibonacciNumber(int n)
+    public static int a(int ab)
     {
-        if ((n == 0) || (n == 1))
+        int c;
+        for (int i = 1; i <= 10; i++)
         {
-            return n;
+            c = ab * i;
+            Console.WriteLine(ab + " * " + i + " = " + c);
         }
-        else
-        {
-            return (NthFibonacciNumber(n - 1) + NthFibonacciNumber(n - 2));
-        }
+        if (ab != 10)
+            ab = a(ab + 1) * ab;
+        return ab;
     }
-    public static long Fibonachi(int n)
+
+    static void PrintReverse(string input)
     {
-        int firstnumber = 0, secondnumber = 1, result = 0;
-        //if (n == 0) return 0; //return the first  number   
-        //if (n == 1) return 1; //return the second  number 
-
-        for (int i = 2; i <= n; i++)
+        if (input.Length > 0)
         {
-            result = firstnumber + secondnumber;
-            firstnumber = secondnumber;
-            secondnumber = result;
+            Console.Write(input[input.Length - 1]);
+            PrintReverse(input.Substring(0, input.Length - 1));
         }
-
-        return result;
+        else Console.Write(".");
     }
-    public static int factorial(int i)
+
+
+    static void Func(int k)
     {
-        int result;
-
-        if (i == 1)
-            return 1;
-        result = factorial(i - 1) * i;
-        return result;
+        if (k < (n / 2))
+            if (n % k == 0)
+                prost = false;
+            else
+            {
+                k++;
+                Func(k);
+            }
     }
+
+    //public static int NthFibonacciNumber(int n)
+    //{
+    //    if ((n == 0) || (n == 1))
+    //    {
+    //        return n;
+    //    }
+    //    else
+    //    {
+    //        return (NthFibonacciNumber(n - 1) + NthFibonacciNumber(n - 2));
+    //    }
+    //}
+    //public static long Fibonachi(int n)
+    //{
+    //    int firstnumber = 0, secondnumber = 1, result = 0;
+    //    //if (n == 0) return 0; //return the first  number   
+    //    //if (n == 1) return 1; //return the second  number 
+
+    //    for (int i = 2; i <= n; i++)
+    //    {
+    //        result = firstnumber + secondnumber;
+    //        firstnumber = secondnumber;
+    //        secondnumber = result;
+    //    }
+
+    //    return result;
+    //}
+    //public static int factorial(int i)
+    //{
+    //    int result;
+
+    //    if (i == 1)
+    //        return 1;
+    //    result = factorial(i - 1) * i;
+    //    return result;
+    //}
 
     public enum aaa
     {
